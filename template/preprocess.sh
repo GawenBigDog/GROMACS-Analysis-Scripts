61e743a53b443486fc518943bcf6c13ffbda57ff
alorkowski <alexander.lorkowski@epfl.ch>
2017-04-10 21:04:01 +0200

    Replaced bad preprocess script with correct one
    	modified:   preprocess.sh

--- template/preprocess.sh
+++ template/preprocess.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
-xtcFile=../data/1mff-last500ns_noPBC.xtc
+xtcFile=../data/1mff-first500ns_noPBC.xtc
 tprFile=../data/topol_noWAT.tpr
 
-./filter.sh $xtcFile $tprFile 1
+gmx make_ndx -f $tprFile -o PO4.ndx <<EOF
+a PO4
+q
+EOF
 
-python leaflet_finder.py ../data/ref_noPBC.pdb "name PO4" l0 l1
-
-gmx trjconv -f $xtcFile -s $tprFile -n l0.ndx -o l0.xtc
-
-gmx trjconv -f $xtcFile -s $tprFile -n l1.ndx -o l1.xtc
-
-gmx trjconv -f $xtcFile -s $tprFile -o prot.xtc
-
-gmx make_ndx -f $tprFile -o PO4.ndx
-
-gmx trjconv -f $xtcFile -s $tprFile -n PO4.ndx -o PO4.xtc
-gmx convert-tpr -s $tprFile -o PO4.tpr -n PO4.ndx
+echo "PO4" | gmx trjconv -f $xtcFile -s $tprFile -n PO4.ndx -o PO4.xtc
+echo "PO4" | gmx convert-tpr -s $tprFile -o PO4.tpr -n PO4.ndx
 echo "0" | gmx trjconv -f PO4.xtc -o PO4.gro -s PO4.tpr -dt 9999999999
